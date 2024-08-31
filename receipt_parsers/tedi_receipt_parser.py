1da7991ab2bbb19950260722e71261908f868f14
smtaha512 <s.m.taha10@gmail.com>
2024-08-31 11:34:41 +0500

    feat: add type definitions

--- receipt_parsers/tedi_receipt_parser.py
+++ receipt_parsers/tedi_receipt_parser.py
@@ -1,19 +1,31 @@
-from datetime import datetime
+from datetime import datetime,_Date
 import re
+from typing import Literal, TypedDict
+
+Item = TypedDict('Item', serial_no=str, name=str, price=float)
+
+ParsedReceipt = TypedDict("ParsedReceipt",
+  items=list[Item],
+  date=_Date,
+  store_name=Literal['Tedi'],
+)
 
 def chunks(items: list[str], chunk_size: int):
   for i in range(0, len(items), chunk_size):
     yield items[i:i + chunk_size]
 
-def parse_receipt(texts: list[str]):
+def parse_receipt(texts: list[str]) -> ParsedReceipt:
   receipt_content = list(map(lambda text: text.description, texts))[0].split("\n")
   
   index_of_first_item = 5
   index_of_last_item_price = receipt_content.index("Kaufsumme:") - 1
   
   items = receipt_content[index_of_first_item : index_of_last_item_price + 1]
-  dict_of_items = list(map(lambda item: { "serial_no": item[0], "name": item[1], "price": float(item[2]) }, list(chunks(items, 3))))
+  item_rows = list(chunks(items, 3))
+  list_of_mapped_items: list[Item] = list(map(lambda item: { "serial_no": item[0], "name": item[1], "price": float(item[2]) }, item_rows))
   
-  transaction_end_at = [item for _, item in enumerate(receipt_content) if re.search('\d{4}-\d{2}-\d{2}T\d{2}', item)][1:2][0]
+  transaction_end_at: str = [item for _, item in enumerate(receipt_content) if re.search('\d{4}-\d{2}-\d{2}T\d{2}', item)][1:2][0]
   
-  return { "items": dict_of_items, "store_name": "Tedi", "date": datetime.strptime(transaction_end_at, '%Y-%m-%dT%H:%M:%S').date() }
+  parsed_receipt: ParsedReceipt = { "items": list_of_mapped_items, "store_name": "Tedi", "date": datetime.strptime(transaction_end_at, '%Y-%m-%dT%H:%M:%S').date() }
+  
+  return parsed_receipt
